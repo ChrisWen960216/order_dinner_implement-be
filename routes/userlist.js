14609574f869629f695d5e85a61cabee116e769a
ChrisWen960216 <chriswen96@163.com>
2017-12-17 17:24:31 +0800

    Add node-canvas,successfully send canvs png to fe

--- routes/userlist.js
+++ routes/userlist.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Canvas = require('canvas'),
+  Image = Canvas.Image,
+  canvas = new Canvas(200, 200),
+  ctx = canvas.getContext('2d');
 
-/* GET users listing. */
 router.get('/', (request, response) => {
-  return response.json({ code: 0, payload: 'HAHA' });
+  ctx.font = '30px Impact';
+  ctx.rotate(0.1);
+  ctx.fillText('Awesome!', 50, 100);
+
+  var te = ctx.measureText('Awesome!');
+  ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+  ctx.beginPath();
+  ctx.lineTo(50, 102);
+  ctx.lineTo(50 + te.width, 102);
+  ctx.stroke();
+
+  // console.log('<img src="' + canvas.toDataURL() + '" />');
+  const url = canvas.toDataURL();
+  console.log(url);
+  return response.send({ code: 0, payload: url });
 });
 
 module.exports = router;
