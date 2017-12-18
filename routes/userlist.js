bf7b56065f63852526792fca4fe564fa026d7259
ChrisWen960216 <chriswen96@163.com>
2017-12-18 09:48:38 +0800

    Add text into png

--- routes/userlist.js
+++ routes/userlist.js
@@ -3,16 +3,12 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const Canvas = require('canvas');
-// const Image = Canvas.Image;
 
-// const out = fs.createWriteStream(path.join(__dirname, '../public/images/form.png'));
-// const stream = canvas.pngStream();
+// router.get('/', (request, response) => {
+//   return response.json({ code: 0, payload: 'HAHA' });
+// });
 
 router.get('/', (request, response) => {
-  return response.json({ code: 0, payload: 'HAHA' });
-});
-
-router.get('/pic', (request, response) => {
   fs.readFile(path.join(__dirname, '../public/images/form.png'), function (error, data) {
     if (error) {
       throw error;
@@ -22,7 +18,14 @@ router.get('/pic', (request, response) => {
 
       const canvas = new Canvas(img.width, img.height);
       const ctx = canvas.getContext('2d');
-      ctx.drawImage(img, 0, 0, img.width / 1.2, img.height / 1.2);
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+      ctx.font = '30px Courier New';
+      // 设置字体填充颜色
+      ctx.fillStyle = 'blue';
+      // 从坐标点(50,50)开始绘制文字
+      // ctx.fillText('卿培', 500, 470);
+      ctx.fillText('卿培', 600, 470);
+      ctx.fillText('卿培', 250, 470);
       const dataUrl = canvas.toDataURL();
       return response.json({ code: 0, payload: dataUrl });
     }
